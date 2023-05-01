297adc7e38232f0fc30c82bb246a607d73c66b96
kadircali <a.kadircali@hotmail.com>
2023-05-01 18:31:20 +0300

    Update Zodyak.java

--- Odevler/src/odevler/Zodyak.java
+++ Odevler/src/odevler/Zodyak.java
@@ -1,22 +1,61 @@
-
 package odevler;
 
 import java.util.Scanner;
 
-
 public class Zodyak {
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Doğum tarihinizi (gün/ay/yıl) formatında giriniz: ");
-        String dt = input.nextLine();
+        System.out.print("Doğum yılınızı giriniz: ");
+        int birthYear = input.nextInt();
 
-        String[] parts = dt.split("/");
-        int year = Integer.parseInt(parts[2]);
+        int zodiacSign = birthYear % 12;
 
-        String[] zodiacs = {"Maymun", "Horoz", "Köpek", "Domuz", "Fare", "Öküz", "Kaplan", "Tavşan", "Ejderha", "Yılan", "At", "Koyun"};
-        String zodiac = zodiacs[year % 12];
+        String signName;
 
-        System.out.println("Çin zodyağı burcunuz: " + zodiac);
+        switch (zodiacSign) {
+            case 0:
+                signName = "Maymun";
+                break;
+            case 1:
+                signName = "Horoz";
+                break;
+            case 2:
+                signName = "Köpek";
+                break;
+            case 3:
+                signName = "Domuz";
+                break;
+            case 4:
+                signName = "Fare";
+                break;
+            case 5:
+                signName = "Öküz";
+                break;
+            case 6:
+                signName = "Kaplan";
+                break;
+            case 7:
+                signName = "Tavşan";
+                break;
+            case 8:
+                signName = "Ejderha";
+                break;
+            case 9:
+                signName = "Yılan";
+                break;
+            case 10:
+                signName = "At";
+                break;
+            case 11:
+                signName = "Koyun";
+                break;
+            default:
+                signName = "Geçersiz";
+                break;
+        }
+
+        System.out.println("Çin Zodyağı burcunuz: " + signName);
     }
 }
