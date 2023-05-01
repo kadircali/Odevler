933c175cb4b1dc590c2f3be76b3693948e589302
kadircali <a.kadircali@hotmail.com>
2023-05-01 21:43:20 +0300

    patika
    kombinasyon hesabı

---
+++ Odevler/src/odevler/Kombinasyon.java
@@ -0,0 +1,50 @@
+package odevler;
+
+import java.util.Scanner;
+
+public class Kombinasyon {
+
+    public static void main(String[] args) {
+        Scanner klavye = new Scanner(System.in);
+        System.out.print("N Değerini Giriniz");
+        int n = klavye.nextInt();
+
+        System.out.print("R Değerini Giriniz");
+        int r = klavye.nextInt();
+
+        if (n < r) {
+
+            System.out.println("N Sayısı R Sayısından Büyük Olmalıdır. Lütfen Tekrar N Değerini Giriniz.");
+            n = klavye.nextInt();
+
+        }
+        int faktoriyel_1 = 1;
+        int faktoriyel_2 = 1;
+        int faktoriyel_3 = 1;
+        int fark = n - r;
+
+        for (int i = 1; i <= n; i++) {
+
+            faktoriyel_1 = faktoriyel_1 * i;
+        }
+        System.out.println("N Sayısının Faktöriyeli : " + faktoriyel_1);
+
+        for (int j = 1; j <= r; j++) {
+
+            faktoriyel_2 = faktoriyel_2 * j;
+        }
+        System.out.println("R Sayısının Faktöriyeli : " + faktoriyel_2);
+
+        for (int k = 1; k <= fark; k++) {
+
+            faktoriyel_3 = faktoriyel_3 * k;
+        }
+        System.out.println("(N-R)'nin Faktöriyeli : " + faktoriyel_3);
+
+        int carpim = faktoriyel_2 * faktoriyel_3;
+        int kombinasyon = faktoriyel_1 / carpim;
+
+        System.out.println(n + " Sayısının " + r + "'li" + " Kombinasyonu = " + kombinasyon);
+    }
+
+}
